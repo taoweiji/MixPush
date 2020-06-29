2b30d400b860d1e3ad1cdf65d7c691f1be7ffdd5
Wiki <taoweiji2008@qq.com>
2020-06-29 17:49:01 +0800

    优化apns

--- mixpush-sender/src/main/java/com/mixpush/sender/provider/MiAPNsPushProvider.java
+++ mixpush-sender/src/main/java/com/mixpush/sender/provider/MiAPNsPushProvider.java
@@ -15,9 +15,7 @@ import org.json.simple.parser.ParseException;
 import java.io.IOException;
 import java.util.List;
 
-/**
- * 如果使用了小米作为默认的透传,那么就不能使用 broadcastNotificationMessageToAll
- */
+
 public class MiAPNsPushProvider extends MiPushProvider {
 
     public MiAPNsPushProvider(String appSecretKey, boolean test) {
@@ -34,6 +32,7 @@ public class MiAPNsPushProvider extends MiPushProvider {
         Message.IOSBuilder builder = new Message.IOSBuilder()
                 .title(mixPushMessage.getTitle())
                 .subtitle(mixPushMessage.getDescription())
+                .extra("payload", mixPushMessage.getPayload())
                 .timeToLive(mixPushMessage.getConfig().getTimeToLive())
                 .timeToSend(mixPushMessage.getTimeToSend());
         return builder.build();
