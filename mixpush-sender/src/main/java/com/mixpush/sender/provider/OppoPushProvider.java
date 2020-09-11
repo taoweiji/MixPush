feac700dff029710ec5e8cfa8973588e77763827
Wiki <taoweiji2008@qq.com>
2020-09-11 14:23:09 +0800

    解决OPPO推送内容过长问题

--- mixpush-sender/src/main/java/com/mixpush/sender/provider/OppoPushProvider.java
+++ mixpush-sender/src/main/java/com/mixpush/sender/provider/OppoPushProvider.java
@@ -99,8 +99,8 @@ public class OppoPushProvider extends MixPushProvider {
         String url = "mixpush://com.mixpush.oppo/message?";
         if (!mixPushMessage.isJustOpenApp()) {
             try {
-                url += "title=" + URLEncoder.encode(mixPushMessage.getTitle(), "utf-8");
-                url += "&description=" + URLEncoder.encode(mixPushMessage.getDescription(), "utf-8");
+//                url += "title=" + URLEncoder.encode(mixPushMessage.getTitle(), "utf-8");
+//                url += "&description=" + URLEncoder.encode(mixPushMessage.getDescription(), "utf-8");
                 url += "&payload=" + URLEncoder.encode(mixPushMessage.getPayload(), "utf-8");
             } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
