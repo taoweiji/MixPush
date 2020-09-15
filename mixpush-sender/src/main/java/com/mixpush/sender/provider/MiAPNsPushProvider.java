8166dbd04577688d5e6c4b43cae2f048e7e4f516
Wiki <taoweiji2008@qq.com>
2020-09-15 11:59:31 +0800

    优化mi_apns 推送

--- mixpush-sender/src/main/java/com/mixpush/sender/provider/MiAPNsPushProvider.java
+++ mixpush-sender/src/main/java/com/mixpush/sender/provider/MiAPNsPushProvider.java
@@ -31,7 +31,6 @@ public class MiAPNsPushProvider extends MiPushProvider {
     protected Message toMessage(MixPushMessage mixPushMessage) {
         Message.IOSBuilder builder = new Message.IOSBuilder()
                 .title(mixPushMessage.getTitle())
-                .subtitle(mixPushMessage.getTitle())
                 .body(mixPushMessage.getDescription())
                 .extra("payload", mixPushMessage.isJustOpenApp() ? "{}" : mixPushMessage.getPayload())
                 .timeToLive(mixPushMessage.getConfig().getTimeToLive())
